515477e8f9b735e7b87714a7df82dd9cb3628345
raghul.saravanan <raghul2521@gmail.com>
2025-01-02 13:03:45 +0530

    Puppeteer aws fix

--- types/executor.ts
+++ types/executor.ts
@@ -1,9 +1,10 @@
 import { Browser, Page } from "puppeteer";
+import { type Browser as BrowserCore, type Page as PageCore } from 'puppeteer-core';
 import { WorkflowTask } from "./workflow";
 import { LogCollector } from "./log";
 
 export type Environment = {
-    browser?: Browser;
+    browser?: Browser | BrowserCore;
     page?: Page;
     //phases with phaseID as key
     phases: {
@@ -18,10 +19,10 @@ export type Environment = {
 export type ExecutionEnvironment<T extends WorkflowTask> = {
     getInput(name: T["inputs"][number]["name"]): string;
     setOutput(name: T["outputs"][number]["name"], value: string): void;
-    getBrowser(): Browser | undefined;
-    setBrowser(browser: Browser): void;
+    getBrowser(): Browser | BrowserCore | undefined;
+    setBrowser(browser: Browser | BrowserCore): void;
     getPage(): Page | undefined;
-    setPage(page: Page): void;
+    setPage(page: Page | PageCore): void;
 
     log: LogCollector;
 }
